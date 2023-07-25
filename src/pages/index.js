5f16414b3d796a645dbc26ac59c1607971f2b5ce
raja8807 <107910012+raja8807@users.noreply.github.com>
2023-07-25 21:36:11 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import HomePage from "@/components/pages/home/home";
 
 const Home = () => {
-  //return <HomePage/>
-return <p> Sorry, This Page is removed </p>
+  return <HomePage/>
+//return <p> Sorry, This Page is removed </p>
 };
 
 export default Home;
